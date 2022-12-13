f8f3414828e220ac5ad0adbd60c7cd4561f0d6d4
Bhavin <bhavin.dholakiya23@gmail.com>
2022-12-13 19:16:13 +0530

    Basics of Arrays

---
+++ src/test/java/JavaBasics/Arrays.java
@@ -0,0 +1,18 @@
+package JavaBasics;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        int num[] = new int[5];
+        int myNum[] = {1,2,3,4,5,8,6,9,8,15};
+        String names[] = {"Bhavin","Jayesh","Mamta","Prayag"};
+
+        num[0] = 1;
+        num[1] = 3;
+        num[2] = 5;
+        num[3] = 7;
+        num[4] = 9;
+
+        System.out.println("Num array index " +num[4]);
+    }
+}
