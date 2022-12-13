d576c1180b6d235f744658bc35b9c99d5bc0fb79
Bhavin <bhavin.dholakiya23@gmail.com>
2022-12-13 19:31:29 +0530

    Basics of For Loops and Enhanced For Loop

---
+++ src/test/java/JavaBasics/ForLoop.java
@@ -0,0 +1,32 @@
+package JavaBasics;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        int num[] = new int[5];
+        int myNum[] = {1,2,3,4,5,8,6,9,8,15};
+        String names[] = {"Bhavin","Jayesh","Mamta","Prayag"};
+
+        num[0] = 1;
+        num[1] = 3;
+        num[2] = 5;
+        num[3] = 7;
+        num[4] = 9;
+
+        for (int i=0; i<num.length;i++)
+        {
+            System.out.println("Num array value " + num[i] + " is at index of " +i);
+        }
+        System.out.println("---------------------------------------------");
+        for (int i=0; i<names.length;i++)
+        {
+            System.out.println("Name " + names[i] + " is at index of " +i);
+        }
+        System.out.println("---------------------------------------------");
+
+        //Enhanced For Loop
+        for (String s : names)
+        {
+            System.out.println(s);
+        }
+    }
+}
