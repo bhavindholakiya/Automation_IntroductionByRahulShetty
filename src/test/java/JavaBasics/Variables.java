fa28827154c079f0e38b40b4ed4a56c5fa1dc1ec
Bhavin <bhavin.dholakiya23@gmail.com>
2022-12-13 19:03:02 +0530

    Basics of Variables.

---
+++ src/test/java/JavaBasics/Variables.java
@@ -0,0 +1,16 @@
+package JavaBasics;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        int myNum = 5;
+        String name = "My name is Bhavin & I'm learning Java";
+        char letter = 'B';
+        double dec = 5.65;
+        boolean isActive = true;
+
+        System.out.println(myNum + " is the value stored in the myNum variable");
+
+        System.out.println(name);
+    }
+}
