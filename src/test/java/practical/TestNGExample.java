12fed2d1937a04d8f615d4ad6a5d10a7fa4cdd56
Bhavin <bhavin.dholakiya23@gmail.com>
2022-12-24 17:47:06 +0530

    Introducing Class name and Css Selector locators to identify elements

--- src/test/java/practical/TestNGExample.java
+++ src/test/java/practical/TestNGExample.java
@@ -2,16 +2,18 @@ package practical;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.BeforeTest;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 public class TestNGExample {
 
@@ -20,7 +22,7 @@ public class TestNGExample {
     public static WebDriver driver = null;
     public static WebDriverWait wait;
 
-    @BeforeTest
+    @BeforeSuite
     public static void browserSetup(){
         if(browser.equalsIgnoreCase("chrome")) {
             System.setProperty("webdriver.chrome.driver", "C:\\Users\\Bhavin\\Downloads\\chromedriver\\chromedriver.exe");
@@ -47,6 +49,41 @@ public class TestNGExample {
         wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class='preloader']")));
         driver.findElement(By.xpath("//a[normalize-space()='Login']")).click();
         System.out.println("Login page url: "+driver.getCurrentUrl());
+    }
+
+    @Test
+    public static void verifyInvalidUsernameAndPassword(){
+        driver.navigate().to("https://rahulshettyacademy.com/locatorspractice/");
+        System.out.println("Navigated to Locators Practice page.");
+
+        // below the element is located by ID attribute
+        WebElement txtUsername = driver.findElement(By.id("inputUsername"));
+        wait.until(ExpectedConditions.elementToBeClickable(txtUsername));
+        txtUsername.sendKeys("bhavin@ekantik.com");
+
+        // below element is located by Name attribute
+        WebElement txtPassword = driver.findElement(By.name("inputPassword"));
+        txtPassword.sendKeys("hello123");
+
+        // below element is locatee by CssSelector
+        // Syntax of CssSelectors are
+        // Class Name -> tagname.classname -> button.signInBtn
+        // Id -> tagname#id -> input#inputUsername
+        // Tagname[attribute='value'] -> input[input='Username']
+
+        WebElement btnSignin = driver.findElement(By.cssSelector("button.signInBtn"));
+        btnSignin.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("p.error")));
+        String expectedErrMsgText = "* Incorrect username or password";
+        String actualErrMsgText = driver.findElement(By.cssSelector("p.error")).getText();
+        Assert.assertEquals(actualErrMsgText, expectedErrMsgText);
+        System.out.println("Actual Error message: "+actualErrMsgText);
+    }
+
+    @AfterSuite
+    public void teadDown(){
         driver.quit();
+        System.out.println("Browser closed.");
     }
 }
