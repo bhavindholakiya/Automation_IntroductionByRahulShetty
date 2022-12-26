5adef66cb2030b02b493ac1424a1882e089e6edb
Bhavin <bhavin.dholakiya23@gmail.com>
2022-12-26 17:37:50 +0530

    Basics of locators and Refactor wait using implicityWait

--- src/test/java/practical/TestNGExample.java
+++ src/test/java/practical/TestNGExample.java
@@ -38,32 +38,31 @@ public class TestNGExample {
             driver = new EdgeDriver();
         }
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get(baseURL);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        //wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     @Test
     public static void lanuchBrowser(){
         System.out.println("Page title: "+driver.getTitle());
         System.out.println("Current page url: "+driver.getCurrentUrl());
-        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class='preloader']")));
+        //wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class='preloader']")));
         driver.findElement(By.xpath("//a[normalize-space()='Login']")).click();
         System.out.println("Login page url: "+driver.getCurrentUrl());
     }
 
-    @Test
+    @Test(priority = 1)
     public static void verifyInvalidUsernameAndPassword(){
         driver.navigate().to("https://rahulshettyacademy.com/locatorspractice/");
         System.out.println("Navigated to Locators Practice page.");
 
         // below the element is located by ID attribute
-        WebElement txtUsername = driver.findElement(By.id("inputUsername"));
-        wait.until(ExpectedConditions.elementToBeClickable(txtUsername));
-        txtUsername.sendKeys("bhavin@ekantik.com");
+        driver.findElement(By.id("inputUsername")).sendKeys("bhavin@ekantik.com");
+        //wait.until(ExpectedConditions.elementToBeClickable(txtUsername));
 
         // below element is located by Name attribute
-        WebElement txtPassword = driver.findElement(By.name("inputPassword"));
-        txtPassword.sendKeys("hello123");
+        driver.findElement(By.name("inputPassword")).sendKeys("hello123");
 
         // below element is locatee by CssSelector
         // Syntax of CssSelectors are
@@ -71,14 +70,20 @@ public class TestNGExample {
         // Id -> tagname#id -> input#inputUsername
         // Tagname[attribute='value'] -> input[input='Username']
 
-        WebElement btnSignin = driver.findElement(By.cssSelector("button.signInBtn"));
-        btnSignin.click();
+        driver.findElement(By.cssSelector("button.signInBtn")).click();
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("p.error")));
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("p.error")));
         String expectedErrMsgText = "* Incorrect username or password";
         String actualErrMsgText = driver.findElement(By.cssSelector("p.error")).getText();
         Assert.assertEquals(actualErrMsgText, expectedErrMsgText);
         System.out.println("Actual Error message: "+actualErrMsgText);
+    }
+
+    @Test(priority = 2)
+    public void navigateToForgotPasswordForm(){
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John Doe");
+        driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("john@hotmail.com");
     }
 
     @AfterSuite
