4b4de44ca10fb91be0f05eb2b13b1d32990bf5b4
Bhavin Dholakiya <bhavin.dholakiya23@gmail.com>
2023-04-11 18:39:21 +0530

    Logs Added and ChromeOptions.

--- src/test/java/practical/CustomizeXpathAndCssSelectors.java
+++ src/test/java/practical/CustomizeXpathAndCssSelectors.java
@@ -3,6 +3,7 @@ package practical;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -10,25 +11,34 @@ public class CustomizeXpathAndCssSelectors {
     public static void main(String[] args) throws InterruptedException {
         String baseURL = "https://rahulshettyacademy.com/";
 
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        
         driver.manage().window().maximize();
+        System.out.println("browser launched");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get(baseURL);
         driver.navigate().to("https://rahulshettyacademy.com/locatorspractice/");
+        System.out.println("Navigate to the website");
         driver.findElement(By.linkText("Forgot your password?")).click();
-
+        System.out.println("Forgot password link clicked");
         //Here, used customized xpath with index of the element.
         driver.findElement(By.xpath("//input[@type='text'][1]")).sendKeys("John Doe");
+        System.out.println("First name entered");
 
         //Here, used customized CssSelector with index of the element.
         driver.findElement(By.cssSelector("input[type='text']:nth-child(3)")).sendKeys("john@hotmail.com");
+        System.out.println("Email address entered");
         Thread.sleep(1000);
 
-        //Cleared entered email address by customized xparth and clear() method.
+        //Cleared entered email address by customized xpath and clear() method.
         driver.findElement(By.xpath(".//input[@type='text'][2]")).clear();
+        System.out.println("Email address cleared.");
         Thread.sleep(1000);
         //Re-enter email address using customized xpath
         driver.findElement(By.cssSelector("input[type='text']:nth-child(3)")).sendKeys("prayag@hotmail.com");
+        System.out.println("New email address entered.");
         Thread.sleep(2000);
 
         driver.quit();
