a29745c599424ca66babc91d4b996a2cfc8d7028
Bhavin Openxcell <bhavin.dholakiya@openxcell.com>
2023-04-13 13:11:34 +0530

    Update methods name for naming convention

--- src/test/java/TestNGTutorial/VerifyLogin.java
+++ src/test/java/TestNGTutorial/VerifyLogin.java
@@ -5,17 +5,27 @@ import org.testng.annotations.Test;
 public class VerifyLogin {
 
     @Test
-    public void WebLogin(){
+    public void LoginViaWeb(){
         System.out.println("Web Login");
     }
 
     @Test
-    public void MobileLogin(){
+    public void LoginViaMobile(){
         System.out.println("Mobile Login");
     }
 
     @Test
     public void LoginViaAPI(){
         System.out.println("Login via API");
+    }
+    
+    @Test
+    public void GoogleAuthentication() {
+    	System.out.println("Login via Google");
+    }
+    
+    @Test
+    public void GoogleDrive() {
+    	System.out.println("Google Drive");
     }
 }
--- src/test/java/TestNGTutorial/VerifySignup.java
+++ src/test/java/TestNGTutorial/VerifySignup.java
@@ -5,17 +5,27 @@ import org.testng.annotations.Test;
 public class VerifySignup {
 
     @Test
-    public void WebSignUp(){
+    public void SignupViaWeb(){
         System.out.println("Web SignUp");
     }
 
     @Test
-    public void MobileSignUp(){
+    public void SigupViaMobile(){
         System.out.println("Mobile SignUp");
     }
 
     @Test
     public void SignUpViaAPI(){
         System.out.println("SignUp via API");
+    }
+    
+    @Test
+    public void GoogleAuthentication() {
+    	System.out.println("Sigup via Google");
+    }
+    
+    @Test
+    public void GoogleDriveSigup() {
+    	System.out.println("Google Drive");
     }
 }
