4c66cb07f7f899fde17dcfce22392df697442074
Bhavin Dholakiya <bhavin.dholakiya23@gmail.com>
2023-06-02 11:12:34 +0530

    Get no. of links and get links inside a specific section

---
+++ src/test/java/practical/WebDriverScope.java
@@ -0,0 +1,27 @@
+package practical;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+public class WebDriverScope {
+    public static void main(String[] args) {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+        List<WebElement> Links = driver.findElements(By.tagName("a"));
+        System.out.println("Available Links: "+Links.size());
+
+        WebElement footerDriver = driver.findElement(By.id("gf-BIG"));
+        List<WebElement> footerLinks = footerDriver.findElements(By.tagName("a"));
+        System.out.println("Footer links : "+footerLinks.size());
+    }
+}
