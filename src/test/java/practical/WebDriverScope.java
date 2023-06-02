30e98c890f7869038683846ed8a9996d8c0a33b4
Bhavin Dholakiya <bhavin.dholakiya23@gmail.com>
2023-06-02 12:33:01 +0530

    Get Number of links present inside 1st block in Footer section.

--- src/test/java/practical/WebDriverScope.java
+++ src/test/java/practical/WebDriverScope.java
@@ -1,6 +1,7 @@
 package practical;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.util.List;
 
 public class WebDriverScope {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         WebDriver driver = new ChromeDriver(options);
@@ -17,11 +18,20 @@ public class WebDriverScope {
         driver.manage().window().maximize();
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 
+        // 1. Get number of links count present in entire page.
         List<WebElement> Links = driver.findElements(By.tagName("a"));
         System.out.println("Available Links: "+Links.size());
 
+        //2. Get number of links present inside only Footer section.
         WebElement footerDriver = driver.findElement(By.id("gf-BIG"));
         List<WebElement> footerLinks = footerDriver.findElements(By.tagName("a"));
         System.out.println("Footer links : "+footerLinks.size());
+
+        //3. Get Number of links present inside 1st block in Footer section.
+        WebElement columnDriver = footerDriver.findElement(By.xpath("//table/tbody/tr/td[1]/ul"));
+        List<WebElement> couponsLink = columnDriver.findElements(By.tagName("a"));
+        System.out.println("Links under 1st section in footer: "+couponsLink.size());
+
+        driver.quit();
     }
 }
