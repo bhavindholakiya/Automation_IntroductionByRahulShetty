94995940968268024dddf7d2115f8016fe13e4d8
Bhavin Dholakiya <bhavin.dholakiya23@gmail.com>
2023-06-13 19:09:50 +0530

    Update GetBrokenLinks.java
    Used openConnection() method from predefined class called URL in java to get broken links.

--- src/test/java/practical/GetBrokenLinks.java
+++ src/test/java/practical/GetBrokenLinks.java
@@ -1,12 +1,17 @@
 package practical;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class GetBrokenLinks {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException, IOException {
         /*
          * Broken URL
          * Step 1 - is to get all urls tied up to the links using Selenium 
@@ -22,7 +27,13 @@ public class GetBrokenLinks {
         driver.manage().window().maximize();
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 
-        String url = driver.findElement(By.cssSelector("a[href*='soapui']")).getAttribute("href");
-        
+        String url = driver.findElement(By.cssSelector("a[href*='brokenlink']")).getAttribute("href");
+
+        HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+        conn.setRequestMethod("HEAD");
+        conn.connect();
+        int resCode = conn.getResponseCode();
+        System.out.println("Response Code: "+resCode);
+        driver.quit();
     }
 }
