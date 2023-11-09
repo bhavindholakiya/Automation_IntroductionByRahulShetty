4a06a0dea3c471c99f35618383b6f611d996bb87
Bhavin Dholakiya <bhavin.dholakiya23@gmail.com>
2023-11-09 00:47:28 -0800

    Uploaded RunningNumber

---
+++ src/test/java/JavaPrograms/FindRunningNumber.java
@@ -0,0 +1,17 @@
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.util.Random;
+
+public class FindRunningNumber {
+	public static final int TIME = 20000;
+    public static final int MAX_Y = 500;
+    public static final int MAX_X = 500;
+    public static void main(String[] args) throws InterruptedException, AWTException {
+        Robot robot = new Robot();
+        Random random = new Random();
+        while(true){
+            robot.mouseMove(random.nextInt(MAX_X), random.nextInt(MAX_Y));
+            Thread.sleep(TIME);
+        }
+    }
+}
