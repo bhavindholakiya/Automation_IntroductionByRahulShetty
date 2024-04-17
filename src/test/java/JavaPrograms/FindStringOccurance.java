19b8dde5a3b16c448378f06f7567e2f738ca7fa6
Bhavin Dholakiya <bhavin.dholakiya23@gmail.com>
2024-04-17 16:32:31 +0530

    Updated with Advance for loop

--- src/test/java/JavaPrograms/FindStringOccurance.java
+++ src/test/java/JavaPrograms/FindStringOccurance.java
@@ -7,14 +7,15 @@ public class FindStringOccurance {
     public static void main(String[] args) {
         String inputString = "Testing is important to find defects and testing is carried to test multiple aspects of application and testing decides quality";
         String expectedString = "Testing";
-        String[] sList = inputString.split(" ");
+        String[] words = inputString.split(" ");
         int count = 0;
 
-        for (int i = 0; i <= sList.length; i++) {
-            if (sList[i] == expectedString) {
+        for (String s : words) {
+            String word = s.toLowerCase();
+            if (word.equalsIgnoreCase(expectedString)) {
                 count++;
             }
         }
-        System.out.println(expectedString + "occurs" + count + "times");
+        System.out.println(expectedString + " occurs " + count + " times");
     }
 }
