9b9305f918e0d5e8d5fd9443e0dd6958bf826bf4
Bhavin Dholakiya <bhavin.dholakiya23@gmail.com>
2024-05-03 12:25:39 +0530

    Added definition of data type

--- src/test/java/JavaBasics/Ch1_Pro1_Variables.java
+++ src/test/java/JavaBasics/Ch1_Pro1_Variables.java
@@ -5,6 +5,15 @@ public class Ch1_Pro1_Variables {
 
         int myNum = 5;
         String name = "My name is Bhavin & I'm learning Java";
+
+        /*
+        * String is non-primitive data type and int is primitive data type
+        * Primitive types are predefined (already defined) in Java.
+        * Non-primitive types are created by the programmer and is not defined by Java (except for String ).
+        * Non-primitive types can be used to call methods to perform certain operations, while primitive types cannot.
+        * String is primitive data type because String is a class with additional capabilities
+        * */
+
         char letter = 'B';
         double dec = 5.65;
         boolean isActive = true;
